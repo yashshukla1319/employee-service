27f67bee4cdf853310e94e123a837aa153a70c72
yashshukla1319 <81147963+yashshukla1319@users.noreply.github.com>
2021-11-23 17:51:56 +0530

    persistence

--- src/main/java/com/ifour/employeeservice/EmployeeService.java
+++ src/main/java/com/ifour/employeeservice/EmployeeService.java
@@ -1,9 +1,6 @@
 package com.ifour.employeeservice;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import java.sql.SQLException;
@@ -14,60 +11,58 @@ public class EmployeeService {
     @Autowired
     public EmployeeRepository employeeRepository;
 
-    HashMap hashMap = new HashMap();
+    @Autowired
+    public PersistenceLayer persistenceLayer;
 
     public List<Employee> getEmployee() throws SQLException {
-        //Pageable pageable = PageRequest.of(no,size);
-        //Page<Employee> employeePage = employeeRepository.getEmployee(pageable);
-        List<Employee> employeePage = new ArrayList<>();
-        return employeeRepository.getEmployee();
+        return persistenceLayer.getEmployee();
     }
 
     public Employee getEmployeeById(Integer id) throws SQLException {
-        Employee exist = employeeRepository.getEmployeeById(id);
+        Employee exist = persistenceLayer.getEmployeeById(id);
         if (exist == null){
             System.out.println("Employee Does not Exist...!");
         }
-        return employeeRepository.getEmployeeById(id);
+        return persistenceLayer.getEmployeeById(id);
     }
 
     public Employee addNewEmployee(Employee employee) throws SQLException {
         Employee search = new Employee();
-        /*Employee getEmployeeById = employeeRepository.getEmployeeById(search.getId());
+        Employee getEmployeeById = persistenceLayer.getEmployeeById(search.getId());
         if (getEmployeeById != null) {
             throw new IllegalStateException("Id already present.");
-        }*/
-        return employeeRepository.addNewEmployee(employee);
+        }
+        return persistenceLayer.addNewEmployee(employee);
     }
 
     public void deleteEmployee(Integer id) throws SQLException {
-        Employee exist = employeeRepository.getEmployeeById(id);
+        Employee exist = persistenceLayer.getEmployeeById(id);
         if (exist == null) {
             throw new IllegalStateException("Employee with Id" + id + "does not exists");
         }
-        employeeRepository.deleteEmployee(id);
+        persistenceLayer.deleteEmployee(id);
     }
 
     @Transactional
     public Employee updateEmployee(Employee employee) throws SQLException {
-        Employee exists = employeeRepository.getEmployeeById(employee.getId());
+        Employee exists = persistenceLayer.getEmployeeById(employee.getId());
         if (exists == null) {
             throw new IllegalStateException("Employee with this id does not exist");
         }
-        return employeeRepository.updateEmployee(employee);
+        return persistenceLayer.updateEmployee(employee);
     }
 
     public List<Employee> findAllByDeptId(Integer deptId) throws SQLException {
-        return employeeRepository.findAllByDeptId(deptId);
+        return persistenceLayer.findAllByDeptId(deptId);
 
     }
 
     public List<Employee> getEmployeeByName(String name) throws SQLException {
-        return employeeRepository.getEmployeeByName(name);
+        return persistenceLayer.getEmployeeByName(name);
     }
 
     public List<Employee> getEmployeeIn(List<Integer> id) throws SQLException {
-        return employeeRepository.findByIdIn(id);
+        return persistenceLayer.findByIdIn(id);
     }
 }
 
